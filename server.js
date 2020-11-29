7e43d27e713d8c6167485586d93415baa5f88ed6
sahil julka <Jhulka_Sahil@cat.com>
2020-11-29 22:50:37 +0530

    add project structure and express server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+
+require("dotenv").config({
+  path: `${path.join(__dirname, "/app/config/.env")}`,
+});
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started listening on ${PORT}`);
+});
+
+console.log(path.join(__dirname, "/public"));
